7bc91427125c386f963a37c3fb856965c7925ff9
KarimElbasiouni <elbasiounikarim@gmail.com>
2024-02-21 01:24:26 -0500

    Add Navigator class - close #9

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team115/Navigator.java
@@ -0,0 +1,26 @@
+package ca.mcmaster.se2aa4.island.team115;
+
+import static org.junit.Assert.assertEquals;
+
+public class Navigator {
+
+    private Direction direction;
+
+    public void changeDirection(){
+        //if statements for changing directions
+    }
+
+    public void navigate(){
+        //Navigates/moves based on direction 
+        switch(direction){
+            case North:
+                break;
+            case East:
+                break;
+            case South:
+                break;
+            case West:
+                break;
+        }
+    }
+}
