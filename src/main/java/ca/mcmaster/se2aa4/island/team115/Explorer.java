5977aa0731c397af5f848e1c29a3db16b5321547
KarimElbasiouni <elbasiounikarim@gmail.com>
2024-03-19 18:02:16 -0400

    Change ExtractInfo and Explorer to reflect new changes to other classes

--- src/main/java/ca/mcmaster/se2aa4/island/team115/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team115/Explorer.java
@@ -10,7 +10,8 @@ import org.json.JSONObject;
 import org.json.JSONTokener;
 
 public class Explorer implements IExplorerRaid {
-
+    private Drone drone;
+    private Translator trans = new Translator();
     private final Logger logger = LogManager.getLogger();
     int i = 0;
     boolean scan = false;
@@ -18,23 +19,13 @@ public class Explorer implements IExplorerRaid {
     @Override
     public void initialize(String s) {
         logger.info("** Initializing the Exploration Command Center");
-        //JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
-        //logger.info("** Initialization info:\n {}",info.toString(2));
-        // String direction = info.getString("heading");
-        // Integer batteryLevel = info.getInt("budget");
-        ExtractInfo extract = new ExtractInfo();
-        try {
-            extract.parser(s);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        
-        Integer batteryLevel = BatteryTracker.getBatteryLevel();
-        //Direction enumDir = extract.getInitialHeading();
-        //String direction = Navigator.enumToString(extract.getInitialHeading()); 
-        //logger.info("The drone is facing {}", direction);
+        JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
+        logger.info("** Initialization info:\n {}",info.toString(2));
+        String direction = info.getString("heading");
+        Integer batteryLevel = info.getInt("budget");
+    
+        drone = new Drone (batteryLevel, direction);
+        logger.info("The drone is facing {}", direction);
         logger.info("Battery level is {}", batteryLevel);
         
         //Direction dir = Navigator.stringToEnum(direction);
@@ -43,42 +34,51 @@ public class Explorer implements IExplorerRaid {
 
     @Override
     public String takeDecision() {
-        
+        //String decisionString = drone.beginExploration();
         JSONObject decision = new JSONObject();
+        decision = drone.beginExploration();
+        // decision.put("action", "echo");
+        // decision.put("parameters", (new JSONObject().put("direction", "E")));
+        
         // decision.put("action", "echo");
         // decision.put("parameters", (new JSONObject()).put("direction", "E"));
         // decision.put("parameters", (new JSONObject()).put("direction", "S"));
-        if(scan){
-            decision.put("action", "scan");
-            scan = false;
-        }else if(i>10){
-            decision.put("action", "stop");
-        }else{
-            decision.put("action", "fly");
-            scan = true;
-            i++;
-        }
+        // if(scan){
+        //     decision.put("action", "scan");
+        //     scan = false;
+        // }else if(i>10){
+        //     decision.put("action", "stop");
+        // }else{
+        //     decision.put("action", "fly");
+        //     scan = true;
+        //     i++;
+        // }
+        //logger.info("** Decision: {}",decisionString);
         logger.info("** Decision: {}",decision.toString());
+        //return decisionString;
         return decision.toString();
     }
 
     @Override
     public void acknowledgeResults(String s) {
-        ExtractInfo extract = new ExtractInfo();
-        try {
-            extract.parser(s);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
         JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
         logger.info("** Response received:\n"+response.toString(2));
-        //Integer cost = response.getInt("cost");
-        Integer cost = extract.getCost();
+        Info information = trans.translate(response);
+
+        Integer cost = information.getCost();
         logger.info("The cost of the action was {}", cost);
-        String status = response.getString("status");
+
+        String status = information.getStatus();
         logger.info("The status of the drone is {}", status);
-        JSONObject extraInfo = response.getJSONObject("extras");
+
+        JSONObject extraInfo = information.getExtras();
         logger.info("Additional information received: {}", extraInfo);
+        
+        drone.updateStatus(cost , status);
+        drone.receiveResponse(information);
+
+    
     }
 
     @Override
