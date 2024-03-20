875ad99a6c019c5ed48f49671fc3900bc2281642
kunxingg <kun.xingg@gmail.com>
2024-03-20 05:43:51 -0400

    new changes to grid search

--- src/main/java/ca/mcmaster/se2aa4/island/team115/IslandGridSearch.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team115/IslandGridSearch.java
@@ -4,6 +4,13 @@ import org.json.JSONTokener;
 
 public class IslandGridSearch {
     public String orientation;
+    private boolean scanning = true;
+    private boolean checkingforward = false;
+    private Integer specialuturnleft = 0;
+    private Integer specialuturnright =0;
+    private Integer uturnright = 0; 
+    private Integer uturnleft = 0;
+    private boolean uturn = false;
     
     Translator translator = new Translator();
     JSONObject response = new JSONObject();
@@ -21,9 +28,9 @@ public class IslandGridSearch {
             case "vertical":
 
             case "horizontal":
-        }
-    }
-    private void alignWithEdge(Direction currdirection){
+       
+    //logic not correct
+    /*private void alignWithEdge(Direction currdirection){
         action.echo(currdirection.leftDir());
         Info leftresponse = translator.translate(response);
         action.echo(currdirection.rightDir());
@@ -36,32 +43,47 @@ public class IslandGridSearch {
             for (int i=0; i<rightrange;i++){
                 action.fly();
             }
-
         }
-       else{
+        else{
             currdirection = currdirection.leftDir();
             action.heading(currdirection);
             for (int i=0; i<leftrange;i++){
                 action.fly();
             }
-        }
-        }
+        }*/
+    }}
         private void specialUTurnRight(Direction currdirection){
-            currdirection = currdirection.rightDir();
-            action.heading(currdirection);
-            action.fly();
-            for(int i=0; i<=2;i++){
+            if (specialuturnright == 0) {
+                currdirection = currdirection.rightDir(); 
+                action.heading(currdirection);
+                specialuturnright++; 
+            } else if (specialuturnright == 1) {
+                action.fly(); 
+                specialuturnright++; 
+            } else if (specialuturnright >= 2 && specialuturnright <= 4) { 
                 currdirection = currdirection.leftDir();
                 action.heading(currdirection);
+                specialuturnright++;
+                if (specialuturnright > 4) {
+                    specialuturnright = 0; 
+                }
             }
         }
         private void specialUTurnLeft(Direction currdirection){
-            currdirection = currdirection.leftDir();
-            action.heading(currdirection);
-            action.fly();
-            for(int i=0; i<=2;i++){
+            if (specialuturnleft == 0) {
+                currdirection = currdirection.leftDir(); 
+                action.heading(currdirection);
+                specialuturnleft++; 
+            } else if (specialuturnleft == 1) {
+                action.fly(); 
+                specialuturnleft++; 
+            } else if (specialuturnleft >= 2 && specialuturnleft <= 4) { 
                 currdirection = currdirection.rightDir();
                 action.heading(currdirection);
+                specialuturnleft++;
+                if (specialuturnleft > 4) {
+                    specialuturnleft = 0; 
+                }
             }
         }
         private void initiate(Direction currdirection){
@@ -74,44 +96,79 @@ public class IslandGridSearch {
             action.heading(currdirection.leftDir());     
         }
         private void uTurnRight(Direction currdirection){
-            for(int i=0; i<=1;i++){
-                action.heading(currdirection.rightDir());
+            if(uturnright == 0){
+                currdirection = currdirection.rightDir();
+                action.heading(currdirection);
+                uturnright++;
             }
+            else if(uturnright == 1){
+                currdirection = currdirection.rightDir();
+                action.heading(currdirection);
+                uturnright++;
+                if (uturnright>1){
+                    uturnright = 0;
+
+                }
+            }      
         }
         private void uTurnLeft(Direction currdirection){
-            for(int i=0; i<=1;i++){
-                action.heading(currdirection.leftDir());
+            if(uturnleft == 0){
+                currdirection = currdirection.leftDir();
+                action.heading(currdirection);
+                uturnleft++;
             }
+            else if(uturnleft == 1){
+                currdirection = currdirection.leftDir();
+                action.heading(currdirection);
+                uturnleft++;
+                if (uturnleft>1){
+                    uturnleft = 0;
+                }
+            }
+            
         }
-        private void gridSearch(Direction currdirection){
-            action.fly();
-            action.scan();
-            while(/*scan is not water*/){
-                action.fly();
+        private JSONObject gridSearch(Direction currdirection) {
+            if(scanning) {
                 action.scan();
+                Info forwardresponse = translator.translate(response);
+                if(/* the scan is water */){
+                    scanning = false ;
+                    checkingforward = false;
+                    uturn = false;  
                 }
-            action.echo(currdirection);
-            Info forwardresponse = translator.translate(response);
-            if(forwardresponse.echoFinding()!=null){
-                Integer range = forwardresponse.echoRange();
-                for(int i;i<range;i++){
-                    action.fly();
-                }
-                gridSearch(currdirection);
-            } 
+                else{
+                    scanning = false;
+                    checkingforward = true;
+                    uturn = false; }
+            } else if(checkingforward) {
+                action.fly();
+                scanning = true;
+                checkingforward = false;
+                uturn = false; 
+                } 
+            else if(uturn){
+                /*call uturn functions, idk right or left 
+                after you uturn you want to start the process again
+                */
+                scanning = true;
+                checkingforward = false;
+                uturn = false; 
             }
-        
-        
-
-        
-
-
-
-
-
-
-
-
-        
-    }
+            else if(!scanning&&!checkingforward) {
+                action.echo(currdirection);
+                Info forwardresponse = translator.translate(response);
+                if(forwardresponse.echoFinding()!=null){
+                    checkingforward = true;
+                    scanning = false;
+                    uturn = false; 
+                    }
+                
+                else{
+                    uturn = true;
+                    checkingforward = false;
+                    scanning = false;
+                }}
+            
+        return action.getDecision(); 
+            }}
 
