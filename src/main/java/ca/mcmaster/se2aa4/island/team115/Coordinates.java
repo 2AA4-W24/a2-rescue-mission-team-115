d4ffcb219ab0b2ab7b81ed52575c08742c86b0ab
KarimElbasiouni <elbasiounikarim@gmail.com>
2024-03-22 23:03:12 -0400

    Change coordinate system to accommodate for fly and heading drone commands

--- src/main/java/ca/mcmaster/se2aa4/island/team115/Coordinates.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team115/Coordinates.java
@@ -1,36 +1,86 @@
 package ca.mcmaster.se2aa4.island.team115;
 
 public class Coordinates {
-    private int x;
-    private int y;
+    private Integer X;
+    private Integer Y;
+    private Direction direction;
 
-    public Coordinates(int x, int y){
-        this.x = x;
-        this.y = y;
+    public Coordinates(Integer X, Integer Y){
+        this.X = X;
+        this.Y = Y;
     }
 
+    public void setDirection (Direction direction){
+        this.direction = direction;
+    }
     public int getX(){
-        return x;
+        return X;
     }
 
     public int getY(){
-        return y;
+        return Y;
     }
 
-    public void incrementX(){
-        x++;
+    public void flyForward(){
+        switch(direction){
+            case Direction.N:
+                Y++;
+                break;
+            case Direction.S:
+                Y--;
+                break;
+            case Direction.E:
+                X++;
+                break;
+            case Direction.W:
+                X--;
+                break;
+
+        }
     }
 
-    public void incrementY(){
-        y++;
+    public void turnLeft(){
+        this.direction = direction.leftDir();
+        switch(direction){
+            case Direction.N:
+                Y++;
+                X--;
+                break;
+            case Direction.S:
+                Y--;
+                X++;
+                break;
+            case Direction.E:
+                Y++;
+                X++;
+                break;
+            case Direction.W:
+                Y--;
+                X--;
+                break;
+        }
+
     }
 
-    public void decrementX(){
-        x--;
+    public void turnRight(){
+        this.direction = direction.leftDir();
+        switch(direction){
+            case Direction.N:
+                Y++;
+                X++;
+                break;
+            case Direction.S:
+                Y--;
+                X--;
+                break;
+            case Direction.E:
+                Y--;
+                X++;    
+                break;
+            case Direction.W:
+                Y++;
+                X--;
+                break;
+        }
     }
-
-    public void decrementY(){
-        y--;
-    }
-
 }
