fc8c5f08decfd6ec60c6e4b1d8cb938b1500e605
kunxingg <kun.xingg@gmail.com>
2024-03-23 23:41:01 -0400

    test

--- src/test/java/ca/mcmaster/se2aa4/island/team115/TranslatorTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team115/TranslatorTest.java
@@ -1,10 +1,7 @@
 package ca.mcmaster.se2aa4.island.team115;
 
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
