c90e2d61b12a7ea8909b43bdde5a82d900789d6d
KarimElbasiouni <elbasiounikarim@gmail.com>
2024-03-24 23:56:19 -0400

    remove unnecessary files

--- src/main/java/ca/mcmaster/se2aa4/island/team115/DroneAct.java
+++
@@ -1,9 +0,0 @@
-package ca.mcmaster.se2aa4.island.team115;
-
-import org.json.JSONObject;
-
-public class DroneAct {
-//    public JSONObject sendDecision{
-//
-//    }
-}
--- src/main/java/ca/mcmaster/se2aa4/island/team115/ScanIsland.java
+++
@@ -1,7 +0,0 @@
-package ca.mcmaster.se2aa4.island.team115;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-public class ScanIsland {
-    
-    }
-
